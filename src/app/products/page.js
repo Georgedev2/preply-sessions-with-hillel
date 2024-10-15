b6a46db6312cb82d2cc8529c27299d8f21899d7f
george <v-gnwobodo@digitalocean.com>
2024-10-15 17:06:21 +0100

    refactored product page

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -4,17 +4,37 @@ import Product from './Product';
 const getProduct = async () => {
   try {
     const res = await fetch('http://localhost:3000/api/products');
-    const body = await res.json();
-    return body.data;
+
+    if (res.status == 200) {
+      const body = await res.json();
+      return {
+        products: body.data,
+        error: null,
+      };
+    }
+    return {
+      products: [],
+      error: {
+        hasError: true,
+        message: `An error with a code of ${res.status} just happens`,
+        status: res.status,
+      },
+    };
   } catch (error) {
-    console.error(error);
-    return [];
+    return {
+      products: [],
+      error: {
+        hasError: true,
+        message: error.message,
+        status: '30000000',
+      },
+    };
   }
 };
 
 const page = async () => {
-  const resBody = await getProduct();
-
+  const { products, error } = await getProduct();
+  // console.log('resBody', resBody);
   return (
     <div className={styles.productPage}>
       <input
@@ -22,11 +42,17 @@ const page = async () => {
         // onChange={handleSearchTerms}
         className={styles.search}
       />
-      <div className={styles.products}>
-        {resBody.map((el, i) => {
-          return <Product productDetail={el} key={i} />;
-        })}
-      </div>
+      {products.length > 0 ? (
+        <div className={styles.products}>
+          {products.map((el, i) => {
+            return <Product productDetail={el} key={i} />;
+          })}
+        </div>
+      ) : (
+        <div>
+          {error.message} 
+        </div>
+      )}
     </div>
   );
 };
@@ -38,3 +64,14 @@ export default page;
 // };
 // const [searchTerm, setSearchTerms] = useState('');
 // const [productFromAPI, setProductFromAPI] = useState([]);
+/* 
+{
+      products: [],
+      error: {
+        hasError: false,
+        message: `An error with a code of ${res.status} happen on the server, please try again latter`,
+        cause: '',
+        code: res.status,
+      },
+    };
+*/
