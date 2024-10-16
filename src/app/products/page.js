719060f48892c3cccdae3118e8b19d37e3eb2947
george <v-gnwobodo@digitalocean.com>
2024-10-16 12:08:31 +0100

    woo commerce added

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,5 +1,13 @@
 import styles from './product.module.css';
 import Product from './Product';
+import WooCommerceRestApi from '@woocommerce/woocommerce-rest-api';
+
+const WooCommerce = new WooCommerceRestApi({
+  url: process.env.DOMAIN, // Your store URL
+  consumerKey: process.env.WOO_COMMERCE_CONSUMER_KEY, // Your consumer key
+  consumerSecret: process.env.WOO_COMMERCE_CONSUMER_SECRET, // Your consumer secret
+  version: 'wc/v3', // WooCommerce WP REST API version
+});
 
 const getProduct = async () => {
   try {
@@ -31,47 +39,66 @@ const getProduct = async () => {
     };
   }
 };
+const getProductFromWooCommerce = async () => {
+  const res = await WooCommerce.get('products?per_page=2');
+
+  // console.log('YES')
+  // console.log('RES WOO 3', res);
+  return res.data;
+};
 
 const page = async () => {
   const { products, error } = await getProduct();
-  // console.log('resBody', resBody);
+  const dataFromWooCommerce = await getProductFromWooCommerce();
+
   return (
-    <div className={styles.productPage}>
-      <input
-        placeholder="Search for products"
-        // onChange={handleSearchTerms}
-        className={styles.search}
-      />
-      {products.length > 0 ? (
+    <div>
+      
+      <section className={styles.productPage}>
+        {/* displaying product from woo commerce */}
+        <h2>Products from woo commerce</h2>
+
         <div className={styles.products}>
-          {products.map((el, i) => {
-            return <Product productDetail={el} key={i} />;
+          {dataFromWooCommerce.map((el, i) => {
+    
+            return (
+              <Product
+                productDetail={{
+                  id: el.id,
+                  price: el.price,
+                  name: el.name,
+                  img: el.images[0].src,
+                  
+                }}
+                key={i}
+              />
+            );
           })}
         </div>
-      ) : (
-        <div>
-          {error.message} 
-        </div>
-      )}
+      </section>
+      <section className={styles.productPage}>
+        <h2>Products from our backend api</h2>
+        {/* displaying product from our API */}
+        <input
+          placeholder="Search for products"
+          // onChange={handleSearchTerms}
+          className={styles.search}
+        />
+
+        {products.length > 0 ? (
+          <div className={styles.products}>
+            {products.map((el, i) => {
+              return <Product productDetail={el} key={i} />;
+            })}
+          </div>
+        ) : (
+          <div>{error.message}</div>
+        )}
+      </section>
+
+ 
     </div>
   );
 };
 
 export default page;
-
-// const handleSearchTerms = (e) => {
-//   setSearchTerms(e.target.value);
-// };
-// const [searchTerm, setSearchTerms] = useState('');
-// const [productFromAPI, setProductFromAPI] = useState([]);
-/* 
-{
-      products: [],
-      error: {
-        hasError: false,
-        message: `An error with a code of ${res.status} happen on the server, please try again latter`,
-        cause: '',
-        code: res.status,
-      },
-    };
-*/
