e46ac92fdf62e3af3009f82e2a7f66d8bbbf0408
george <v-gnwobodo@digitalocean.com>
2024-10-20 11:22:48 +0100

    clearn up

--- src/app/products/[productID]/page.js
+++ src/app/products/[productID]/page.js
@@ -1,34 +1,40 @@
+
 import { products } from '../../data';
 import { wooCommerce } from '../../components/utils/wooCommerceUtils';
 import ProductVariation from '../ProductVariation';
-import SubscriptionTerm  from './SubscriptionTerm';
+import SubscriptionTerm from './SubscriptionTerm';
 
 const wooProductVariations = async (productId) => {
-  
+  // 10614
+  console.log('parentID', productId);
   try {
-  const res = await wooCommerce.get(`products/${productId}/variations`) 
-  //`products/${productId}/variations`);
-  console.log('res-', res)
-  if (!res.data) {
-  throw new Error('No data found in response');
-  }
-  
-  const variations = res.data;
-  
-  const result = variations.map(variation => ({
-  parent_id: variation.parent_id,
-  sku: variation.sku,
-  id: variation.id,
-  price: variation.price,
-  attributes: variation.attributes
-  }));
-  return  result ;
-  // console.log(JSON.stringify(result, null, 2));
+    const res = await wooCommerce.get(`products/${productId}/variations`);
+    if (!res.data) {
+      throw new Error('No data found in response');
+    }
+
+    const variations = res.data;
+    console.log('res.data', res.data);
+    const result = variations.map((variation) => {
+      console.log('    attributes', variation.attributes);
+
+      return {
+        parent_id: variation.parent_id,
+        sku: variation.sku,
+        id: variation.id,
+        price: variation.price,
+        attributes: variation.attributes,
+        image: variation.image.src,
+      };
+    });
+    console.log('result ', result);
+    return result;
+    // console.log(JSON.stringify(result, null, 2));
   } catch (error) {
-  console.error('Error fetching variations:', error);
+    console.error('Error fetching variations:', error);
   }
-  };
-  
+};
+
 const getProductById = async (productId) => {
   try {
     const res = await wooCommerce.get(`products/${productId}`);
@@ -45,21 +51,21 @@ const getProductById = async (productId) => {
     };
   }
 };
-const getPrice=()=>{
-  return 0
-}
+
 const ProductDetailPage = async (props) => {
   const { params } = props;
   const productId = parseInt(params.productID);
   const { product, error } = await getProductById(productId);
-  const variationPP =await   wooProductVariations(productId);////
-  console.log('variationPPP', variationPP)
-
+  const variationPP = await wooProductVariations(productId); ////
+  // console.log('variationPPP', variationPP)
+  // console.log(' variationPP ', variationPP )
   const variation = product
     ? product.attributes.map((attribute) => {
         return attribute.options;
       })
     : [];
+
+  // console.log(' variation', variation)
   return (
     <div>
       {product ? (
@@ -69,17 +75,29 @@ const ProductDetailPage = async (props) => {
             <p>{product.name}</p>
             <p>{product.description}</p>
           </div>
-          <SubscriptionTerm  variation={variation} price={product.price}/>
+          <SubscriptionTerm variation={variation} price={product.price} />
           {product.images.map((image) => {
             return (
               <img key={image.id} src={image.src} width={80} height={80} />
             );
           })}
           <div>
-    
             <div>
               <b>Colors: </b>
-              <ProductVariation variation={variation[1]} />
+              
+          
+              <ProductVariation
+                variation={variation[1]}
+              />
+            </div>
+            <div>
+              {variationPP.map((item, i) => {
+                return (
+                  <span key={i}>
+                    <img src={item.image} alt="" width={80} height={50} />
+                  </span>
+                );
+              })}
             </div>
           </div>
         </div>
