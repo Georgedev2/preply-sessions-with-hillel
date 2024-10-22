96b3410526fdfc06b3489f73f528987637288d5e
george <v-gnwobodo@digitalocean.com>
2024-10-22 11:54:17 +0100

    fixed product detail page

--- src/app/products/[productID]/SubscriptionTerm.js
+++ src/app/products/[productID]/SubscriptionTerm.js
@@ -1,6 +1,4 @@
-'use client';
 import React, { useState } from 'react';
-import ProductVariation from '../ProductVariation';
 
 const SubscriptionTerm = (props) => {
   const { variation, price } = props;
--- src/app/products/[productID]/page.js
+++ src/app/products/[productID]/page.js
@@ -1,8 +1,7 @@
 
 import { products } from '../../data';
 import { wooCommerce } from '../../components/utils/wooCommerceUtils';
-import ProductVariation from '../ProductVariation';
-import SubscriptionTerm from './SubscriptionTerm';
+import ProductDetail  from './ProductDetail';
 
 const wooProductVariations = async (productId) => {
   // 10614
@@ -56,56 +55,17 @@ const ProductDetailPage = async (props) => {
   const { params } = props;
   const productId = parseInt(params.productID);
   const { product, error } = await getProductById(productId);
-  const variationPP = await wooProductVariations(productId); ////
-  // console.log('variationPPP', variationPP)
-  // console.log(' variationPP ', variationPP )
-  const variation = product
-    ? product.attributes.map((attribute) => {
-        return attribute.options;
-      })
-    : [];
+  const productVariations = await wooProductVariations(productId); ////
 
-  // console.log(' variation', variation)
   return (
     <div>
       {product ? (
-        <div>
-          <img src={product.images[0].src} alt="" width={200} height={100} />
-          <div>
-            <p>{product.name}</p>
-            <p>{product.description}</p>
-          </div>
-          <SubscriptionTerm variation={variation[0]} price={product.price} />
-          {product.images.map((image) => {
-            return (
-              <img key={image.id} src={image.src} width={80} height={80} />
-            );
-          })}
-          <div>
-            <div>
-              <b>Colors: </b>
-              
-          
-              <ProductVariation
-                variation={variation[1]}
-              />
-            </div>
-            <div>
-              {variationPP.map((item, i) => {
-                return (
-                  <span key={i}>
-                    <img src={item.image} alt="" width={80} height={50} />
-                  </span>
-                );
-              })}
-            </div>
-          </div>
-        </div>
+    <ProductDetail product={product} productVariations={productVariations}/>
       ) : (
         <div> {error.message}</div>
       )}
     </div>
   );
 };
-// [['Black', 'Green'], ['S', 'M']]
+
 export default ProductDetailPage;
