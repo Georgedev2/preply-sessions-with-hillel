43367c33359f2a7e67aa4fc9a34e8ab1b125bbe5
george <v-gnwobodo@digitalocean.com>
2024-10-26 14:31:16 +0100

    add the product id and name to the URL

---
+++ src/app/products/(productListing)/page.js
@@ -0,0 +1,102 @@
+import styles from './product.module.css';
+import wooCommerce from '../../components/utils/wooCommerceUtils';
+import Link from 'next/link';
+import Image from 'next/image';
+
+const getProduct = async () => {
+  try {
+    const res = await fetch('http://localhost:3000/api/products');
+
+    if (res.status == 200) {
+      const body = await res.json();
+      return {
+        products: body.data,
+        error: null,
+      };
+    }
+    return {
+      products: [],
+      error: {
+        hasError: true,
+        message: `An error with a code of ${res.status} just happens`,
+        status: res.status,
+      },
+    };
+  } catch (error) {
+    return {
+      products: [],
+      error: {
+        hasError: true,
+        message: error.message,
+        status: '30000000',
+      },
+    };
+  }
+};
+
+const getProductFromWooCommerce = async () => {
+  try {
+    const res = await wooCommerce.get('products?per_page=15');
+    // woo commerce by default throws error if the status is not 200
+    if (res.status == 200) {
+      return {
+        products: res.data,
+        error: null,
+      };
+    }
+  } catch (error) {
+    return {
+      products: [],
+      error: {
+        message: error.message,
+      },
+    };
+  }
+};
+
+const productListingPage = async (props) => {
+  const { products, error } = await getProductFromWooCommerce();
+
+  const createProductURL = (name, id) => {
+    if (name && id) {
+      return `/products/${name.replaceAll(' ', '-')}-${id}`.toLowerCase();
+    }
+    return '';
+  };
+
+  return (
+    <div>
+      <section className={styles.productPage}>
+        <h2>Products</h2>
+        <div className={styles.products}>
+          {products.length > 0 ? (
+            products.map((el) => {
+              return (
+                <Link href={`${createProductURL(el.name, el.id)}`} key={el.id}>
+                  <Image
+                    src={el.images[0].src}
+                    alt=""
+                    width={200}
+                    height={100}
+                  />
+                  <div>
+                    <p>{el.name}</p>
+                    <p>{el.price}</p>
+                  </div>
+                </Link>
+              );
+            })
+          ) : (
+            <div>
+              {error.message}, Please try again later
+            </div>
+          )}
+        </div>
+      </section>
+   
+    </div>
+  );
+};
+// Sorry, we couldn't find the page you're looking for.
+export default productListingPage;
+
--- src/app/products/[productID]/page.js
+++ src/app/products/[productID]/page.js
@@ -1,36 +1,35 @@
-import { products } from '../../data';
 import { wooCommerce } from '../../components/utils/wooCommerceUtils';
 import ProductDetail from './ProductDetail';
-import { describe } from 'node:test';
+import Image from 'next/image';
 
 const wooProductVariations = async (productId) => {
   // 10614
-  // console.log('parentID', productId);
   try {
     const res = await wooCommerce.get(`products/${productId}/variations`);
-    if (!res.data) {
-      throw new Error('No data found in response');
-    }
 
-    const variations = res.data;
-    console.log('DATA=', res.data);
-    const result = variations.map((variation) => {
-      // console.log('    attributes', variation.attributes);
-
+    if (res.status == 200) {
+      const result = res.data.map((variation) => {
+        return {
+          parent_id: variation.parent_id,
+          sku: variation.sku,
+          id: variation.id,
+          price: variation.price,
+          attributes: variation.attributes,
+          image: variation.image.src,
+        };
+      });
       return {
-        parent_id: variation.parent_id,
-        sku: variation.sku,
-        id: variation.id,
-        price: variation.price,
-        attributes: variation.attributes,
-        image: variation.image.src,
+        productVariations: result,
+        error: null,
       };
-    });
-    // console.log('result', result);
-    return result;
-    // console.log(JSON.stringify(result, null, 2));
+    }
   } catch (error) {
-    console.error('Error fetching variations:', error);
+    return {
+      productVariations: null,
+      error: {
+        message: error.message,
+      },
+    };
   }
 };
 
@@ -53,31 +52,44 @@ const getProductById = async (productId) => {
 
 const ProductDetailPage = async (props) => {
   const { params } = props;
-  const productId = parseInt(params.productID);
+  const m = params.productID.split('-');
+  const productId = m[m.length - 1];
   const { product, error } = await getProductById(productId);
-  const productVariations = await wooProductVariations(productId); ////
+  const { productVariations}= await wooProductVariations(productId); ////
 
   const attributes = product
-  ? product.attributes.map((attribute) => {
-      return attribute.options;
-    })
-  : []; // attribute could either be color or subscription
-  // console.log(' productVariations', productVariations[0].  attributes);
-
-// console.log('productRAW',product. attributes[0])
+    ? product.attributes.map((attribute) => {
+        return attribute.options;
+      })
+    : []; // attribute could either be color or subscription
+// console.log('productVariationsBBB',productVariations)
+// console.log('productId',productId)
   return (
     <div>
       {product ? (
-        <ProductDetail
-          product_={{
-            name: product.name,
-            description: product.description,
-            image: product.images[0].src,
-            price: product.price,
-            attributes: attributes
-          }}
-          productVariations={productVariations}
-        />
+        <>
+          <ProductDetail
+            product_={{
+              name: product.name,
+              description: product.description,
+              image: product.images[0].src,
+              price: product.price,
+              attributes: attributes,
+            }}
+            productVariations={ productVariations}
+          />
+          <div>
+            {product.images.map((image) => {
+              <Image
+                src={image.src}
+                key={image.id}
+                width={50}
+                alt=""
+                height={50}
+              />;
+            })}
+          </div>
+        </>
       ) : (
         <div> {error.message}</div>
       )}
